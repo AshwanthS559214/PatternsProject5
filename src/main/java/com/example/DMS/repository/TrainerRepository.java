a11f1625e4844cc328dcc6f0daebd4e19e0318c8
Ashwanth_s559214 <123043659+AshwanthS559214@users.noreply.github.com>
2023-11-26 00:00:36 -0600

    Update TrainerRepository.java

--- src/main/java/com/example/DMS/repository/TrainerRepository.java
+++ src/main/java/com/example/DMS/repository/TrainerRepository.java
@@ -1,10 +1,10 @@
 /**
  * @aswanth
  */
-package com.example.DMS.Controllers;
+package com.example.DMS.repository;
 import org.springframework.data.repository.CrudRepository;
 
-import com.bandiClasses.DMS.Models.Trainer;
+import com.example.DMS.Models.Trainer;
 
 public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
 	
